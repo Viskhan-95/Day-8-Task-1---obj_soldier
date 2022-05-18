b6be0a0ed1ec5ef4345a482518d7b4af88102ca6
viskhan <viskhan.rkb@gmail.com>
2022-05-18 10:41:22 +0300

    доработал методы объекта

--- soldier.js
+++ soldier.js
@@ -1,13 +1,35 @@
 const soldier = {
     name: "Халк",
-    health: 9,
+    health: 10,
     weapon: {
         name: "АКМ",
-        cartridges: 30,
+        patrons: 30,
     },
     supplies: 3,
 
-    fire: function () {},
-    recharge: function () {},
-    hurt: function () {},
+    fire: function () {
+        patrons--;
+        console.log('бах-бах');
+        if(patrons === 0) {
+            console.log('Обойма пуста. Перезаредитесь');
+        }
+    },
+
+    recharge: function () {
+        patrons = 30;
+        this.supplies--;
+        
+        console.log('Перезарядка');
+
+        if(this.supplies === 0) {
+            console.log('Не осталось припасов');
+        }
+    },
+
+    hurt: function () {
+        this.health--;
+        if(this.health === 0) {
+            console.log('Ты проиграл')
+        }
+    },
 };
