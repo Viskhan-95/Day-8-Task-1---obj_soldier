96b1a64955515cfd6e0a6b29024ca1d7d1c97590
viskhan <viskhan.rkb@gmail.com>
2022-05-18 10:31:29 +0300

    создал модель солдата

---
+++ soldier.js
@@ -0,0 +1,13 @@
+const soldier = {
+    name: "Халк",
+    health: 9,
+    weapon: {
+        name: "АКМ",
+        cartridges: 30,
+    },
+    supplies: 3,
+
+    fire: function () {},
+    recharge: function () {},
+    hurt: function () {},
+};
